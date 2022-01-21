f7aa8a09cba358f858712ff126e68ccd52226689
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-01-21 14:08:38 -0300

    Criado a controller para criação de filme no db

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,7 +1,16 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 
+import Logger from "../../config/logger";
 import { MovieModel } from "../models/Movie";
 
 export async function createMovie(req: Request, res: Response) {
-  return res.status(200).send("Controller ok");
+  try {
+    const data = req.body;
+    const movie = await MovieModel.create(data);
+    return res.status(200).json(movie);
+  } catch (error: any) {
+    Logger.error(`Erro no sistema: ${error.message}`);
+  }
 }
